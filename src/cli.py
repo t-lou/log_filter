3bfa0125f4cb93b3064406f3f918da5b038fc22f
Tongxi Lou <tongxi.lou@gmail.com>
2025-12-20 12:06:47 +0100

    add export function and refine cli export

--- src/cli.py
+++ src/cli.py
@@ -23,34 +23,29 @@ def parse_cli_args():
     return parser.parse_args()
 
 
-def main_cli(filters: dict[str, Filter]) -> None:
-    args = parse_cli_args()
-
-    print("Input file:", args.input_file, type(args.input_file))
-    print("Output dir:", args.output_dir, type(args.output_dir))
-
+def filter_logs(filters: dict[str, Filter], input_file: Path, output_dir: Path) -> None:
     # Check input
-    assert args.input_file.exists(), f"Input file {args.input_file} doesn't exist."
+    assert input_file.exists(), f"Input file {input_file} doesn't exist."
 
     # Clear output
-    if args.output_dir.exists():
-        assert args.output_dir.is_dir(), f"Existing output path {args.output_dir} is a file."
-        print(f"Remove {args.output_dir}")
-        shutil.rmtree(args.output_dir)
+    if output_dir.exists():
+        assert output_dir.is_dir(), f"Existing output path {output_dir} is a file."
+        print(f"Remove {output_dir}")
+        shutil.rmtree(output_dir)
 
-    args.output_dir.mkdir()
+    output_dir.mkdir(parents=True, exist_ok=True)
 
     # Stream through the file line by line
     outputs = []
     ordered_filters = []
     for name, filters in filters.items():
-        f = (args.output_dir / (name + ".txt")).open("w", encoding="utf-8")
+        f = (output_dir / (name + ".txt")).open("w", encoding="utf-8")
         outputs.append(f)
         ordered_filters.append(filters)
 
     try:
         # Stream input line by line
-        with args.input_file.open("r", encoding="utf-8", errors="ignore") as f:
+        with input_file.open("r", encoding="utf-8", errors="ignore") as f:
             for line in f:
                 stripped = line.strip()
                 if not stripped:
@@ -63,3 +58,12 @@ def main_cli(filters: dict[str, Filter]) -> None:
         # Close all outputs
         for f in outputs:
             f.close()
+
+
+def main_cli(filters: dict[str, Filter]) -> None:
+    args = parse_cli_args()
+
+    print("Input file:", args.input_file, type(args.input_file))
+    print("Output dir:", args.output_dir, type(args.output_dir))
+
+    filter_logs(filters=filters, input_file=args.input_file, output_dir=args.output_dir)
--- src/gui.py
+++ src/gui.py
@@ -1,3 +1,4 @@
+import shutil
 import tkinter as tk
 from pathlib import Path
 from tkinter import filedialog, scrolledtext, ttk
@@ -32,8 +33,8 @@ def load_file(filters: dict[str, Filter], text_widgets: dict[str, scrolledtext.S
             if not stripped:
                 continue
 
-            # Always show in "Original"
-            text_widgets["Original"].insert(tk.END, stripped + "\n")
+            # Always show in "original"
+            text_widgets["original"].insert(tk.END, stripped + "\n")
 
             # Apply filters
             for tab_name, tab_filters in filters.items():
@@ -43,6 +44,25 @@ def load_file(filters: dict[str, Filter], text_widgets: dict[str, scrolledtext.S
     # Lock the text boxes to forbid editing
     for text_widget in text_widgets.values():
         text_widget.config(state="disabled")
+
+
+def save_to(text_widgets: dict[str, scrolledtext.ScrolledText]):
+    folder = filedialog.askdirectory(title="Select or create directory to save the sub-logs")
+
+    path = Path(folder)
+    if not folder or not path.exists() or not path.is_dir():
+        return
+
+    for item in path.iterdir():
+        if item.is_file() or item.is_symlink():
+            item.unlink()
+        elif item.is_dir():
+            shutil.rmtree(item)
+
+    for name, widget in text_widgets.items():
+        content = widget.get("1.0", "end-1c")
+        with (path / (name + ".txt")).open("w", encoding="utf-8") as f:
+            f.write(content)
 
 
 def main_gui(filters: dict[str, Filter]) -> None:
@@ -58,13 +78,13 @@ def main_gui(filters: dict[str, Filter]) -> None:
     notebook = ttk.Notebook(root)
     notebook.pack(fill="both", expand=True)
 
-    # Predefined "Original" tab
+    # Predefined "original" tab
     text_widgets = {}
     frame = ttk.Frame(notebook)
-    notebook.add(frame, text="Original")
+    notebook.add(frame, text="original")
     text_area = scrolledtext.ScrolledText(frame, wrap=tk.WORD)
     text_area.pack(fill="both", expand=True)
-    text_widgets["Original"] = text_area
+    text_widgets["original"] = text_area
 
     # Create tabs dynamically from filters
     for flt in filters.keys():
@@ -78,6 +98,7 @@ def main_gui(filters: dict[str, Filter]) -> None:
     menubar = tk.Menu(root)
     file_menu = tk.Menu(menubar, tearoff=0)
     file_menu.add_command(label="Open File", command=lambda: load_file(filters, text_widgets, root))
+    file_menu.add_command(label="Save", command=lambda: save_to(text_widgets))
     file_menu.add_separator()
     file_menu.add_command(label="Exit", command=root.quit)
     menubar.add_cascade(label="File", menu=file_menu)
